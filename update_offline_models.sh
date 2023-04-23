ff6647153470609c935fb2a876e950087730c057
Stephen Hudson <stephen.donald.hudson@gmail.com>
2023-04-23 12:51:50 -0400

    Automated a few steps for updating offline models

--- update_offline_models.sh
+++ update_offline_models.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-kaggle datasets version -p ./birdclef-2023-offline-models -m "$1"
+model_dir="birdclef-2023-offline-models"
+
+
+rm ./$model_dir/*.pkl
+new_model=`ls -rt *.pkl | grep -v indices.pkl$ | tail -1`
+echo "Sending $new_model to kaggle offline models dataset"
+cp $new_model ./$model_dir/
+kaggle datasets version -p ./$model_dir -m "$1"
+mv *.pkl ./models/
